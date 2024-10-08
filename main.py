9cd8cddf41d18e2e00ef96b55adafbadeb20cf07
cantin-bruno <cantin.bruno@edu.esiee.fr>
2024-10-08 07:43:30 +0000

    Syracuse

--- main.py
+++ main.py
@@ -23,65 +23,36 @@ def syr_plot(lsyr):
 #######################
 
 def syracuse_l(n):
-    """retourne la suite de Syracuse de source n
 
-    Args:
-        n (int): la source de la suite
-
-    Returns:
-        list: la suite de Syracuse de source n
-    """
-
-    # votre code ici 
-    l = [ ]
+    l = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        l.append(n)
     return l
 
 def temps_de_vol(l):
-    """Retourne le temps de vol d'une suite de Syracuse
-
-    Args:
-        l (list): la suite de Syracuse
-
-    Returns:
-        int: le temps de vol
-    """
     
-    # votre code ici
-
-    n = 0
+    n=len(l) - 1
     return n
 
 def temps_de_vol_en_altitude(l):
-    """Retourne le temps de vol en altitude d'une suite de Syracuse
 
-    Args:
-        l (list): la suite de Syracuse
-
-    Returns:
-        int: le temps de vol en altitude
-    """
-
-    # votre code ici
-
-    n = 0
-    return n
-
+    n = l[0]
+    p = 0
+    for x in l:
+        if x > n:
+            p += 1
+        else:
+            break
+    return p
 
 def altitude_maximale(l):
-    """retourne l'altitude maximale d'une suite de Syracuse
-
-    Args:
-        l (list): la suite de Syracuse
-
-    Returns:
-        int: l'altitude maximale
-    """
     
-    # votre code ici
-    
-    n = 0
+    n =max(l)
     return n
-
 
 #### Fonction principale
 
